c7c024a448c61646339140e16075bd035f1201f6
Brad Wise <brad@foghornconsulting.com>
2019-12-09 10:14:06 -0600

    Adding s3 bucket and bucket policy resource

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bw-sentinel" {
+    bucket = "bw-sentinel"
+    region = "us-east-1"
+
+    server_side_encryption_configuration {
+        rule {
+          apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+         }
+       }
+    }
+}
+
+resource "aws_s3_bucket_policy" "airbnb-brad-internal-only" {
+    bucket = "${aws_s3_bucket.bw-sentinel.id}"
+    policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
+}
+
+data "aws_iam_policy_document" "bw-sentinel-policy" {
+  statement {
+    effect    = "Deny"
+    actions   = ["s3:*"]
+    resources = [
+      "arn:aws:s3:::bw-sentinel/*",
+      "arn:aws:s3:::bw-sentinel"
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
