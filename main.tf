6d2eba0682087896c6b9e29f3c4d95d558f08115
Brad Wise <brad@foghornconsulting.com>
2019-12-11 14:53:09 -0600

    test sentinel policy with aws_iam_policy resource

--- main.tf
+++ main.tf
@@ -2,49 +2,67 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_kms_key" "bw-sentinel" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 7
+resource "aws_iam_policy" "cloudfront-cache-management" {
+  name_prefix = "ccm-btdghub-cf-prod"
+  description = "Policy to allow instances to invalidate CloudFront distribution cache objects"
+  policy      = "${data.aws_iam_policy_document.cloudfront-cache-management-policy-document.json}"
 }
 
-resource "aws_s3_bucket" "bw-sentinel" {
-    bucket = "bw-sentinel"
-    region = "us-east-1"
-
-    server_side_encryption_configuration {
-        rule {
-          apply_server_side_encryption_by_default {
-            kms_master_key_id = "${aws_kms_key.bw-sentinel.arn}"
-            # Good algorithm
-            sse_algorithm = "aws:kms"
-            # Bad algorithm
-            #sse_algorithm = "AES256"
-         }
-       }
-    }
-}
-
-resource "aws_s3_bucket_policy" "airbnb-brad-internal-only" {
-    bucket = "${aws_s3_bucket.bw-sentinel.id}"
-    policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
-}
-
-# Bad policy
-data "aws_iam_policy_document" "bw-sentinel-policy" {
+data "aws_iam_policy_document" "cloudfront-cache-management-policy-document" {
   statement {
-    effect    = "Allow"
-    actions   = ["s3:*"]
-    resources = [
-      "arn:aws:s3:::bw-sentinel/*",
-      "arn:aws:s3:::bw-sentinel"
+    actions = [
+      "cloudfront:CreateInvalidation",
+      "cloudfront:GetInvalidation",
     ]
-
-    principals {
-      type        = "*"
-      identifiers = ["*"]
-    }
+    resources = [
+      "*",
+    ]
   }
 }
+
+#resource "aws_kms_key" "bw-sentinel" {
+#  description             = "This key is used to encrypt bucket objects"
+#  deletion_window_in_days = 7
+#}
+#
+#resource "aws_s3_bucket" "bw-sentinel" {
+#    bucket = "bw-sentinel"
+#    region = "us-east-1"
+#
+#    server_side_encryption_configuration {
+#        rule {
+#          apply_server_side_encryption_by_default {
+#            kms_master_key_id = "${aws_kms_key.bw-sentinel.arn}"
+#            # Good algorithm
+#            sse_algorithm = "aws:kms"
+#            # Bad algorithm
+#            #sse_algorithm = "AES256"
+#         }
+#       }
+#    }
+#}
+#
+#resource "aws_s3_bucket_policy" "airbnb-brad-internal-only" {
+#    bucket = "${aws_s3_bucket.bw-sentinel.id}"
+#    policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
+#}
+#
+## Bad policy
+#data "aws_iam_policy_document" "bw-sentinel-policy" {
+#  statement {
+#    effect    = "Allow"
+#    actions   = ["s3:*"]
+#    resources = [
+#      "arn:aws:s3:::bw-sentinel/*",
+#      "arn:aws:s3:::bw-sentinel"
+#    ]
+#
+#    principals {
+#      type        = "*"
+#      identifiers = ["*"]
+#    }
+#  }
+#}
 
 # Good Policy
 #data "aws_iam_policy_document" "bw-sentinel-policy" {
