dec07af2666f0c7134616c977ddf9d13a96e0367
Brad Wise <brad@foghornconsulting.com>
2019-12-18 15:13:11 -0600

    fixes

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
-#resource "aws_kms_key" "bw-sentinel" {
-#  description             = "This key is used to encrypt bucket objects"
-#  deletion_window_in_days = 7
-#}
+resource "aws_kms_key" "bw-sentinel" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 7
+}
 
 resource "aws_s3_bucket" "bw-sentinel" {
     bucket = "bw-sentinel"
