5610cea16f0d5731b273e98829414ea160fc0358
Brad Wise <brad@foghornconsulting.com>
2019-12-18 15:07:12 -0600

    remove iam resource add bucket resources

--- iam.tf
+++ iam.tf
@@ -16,23 +16,23 @@
 #  }
 #}
 
-resource "aws_iam_user" "sentinel-test-user" {
-  name          = "sentinel-test-user"
-  force_destroy = true
-}
-
-resource "aws_iam_role" "sentinel-test-role" {
-  name               = "sentinel-test-role"
-  assume_role_policy = "${data.aws_iam_policy_document.sentinel-test-role-assume-policy.json}"
-}
-
-data "aws_iam_policy_document" "sentinel-test-role-assume-policy" {
-  statement {
-    effect  = "Allow"
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
+#resource "aws_iam_user" "sentinel-test-user" {
+#  name          = "sentinel-test-user"
+#  force_destroy = true
+#}
+#
+#resource "aws_iam_role" "sentinel-test-role" {
+#  name               = "sentinel-test-role"
+#  assume_role_policy = "${data.aws_iam_policy_document.sentinel-test-role-assume-policy.json}"
+#}
+#
+#data "aws_iam_policy_document" "sentinel-test-role-assume-policy" {
+#  statement {
+#    effect  = "Allow"
+#    actions = ["sts:AssumeRole"]
+#    principals {
+#      type        = "Service"
+#      identifiers = ["ec2.amazonaws.com"]
+#    }
+#  }
+#}
--- s3.tf
+++ s3.tf
@@ -19,12 +19,12 @@
 #       }
 #    }
 #}
-#
-#resource "aws_s3_bucket_policy" "airbnb-brad-internal-only" {
-#    bucket = "${aws_s3_bucket.bw-sentinel.id}"
-#    policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
-#}
-#
+
+resource "aws_s3_bucket_policy" "airbnb-brad-internal-only" {
+    bucket = "${aws_s3_bucket.bw-sentinel.id}"
+    policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
+}
+
 ## Bad policy
 #data "aws_iam_policy_document" "bw-sentinel-policy" {
 #  statement {
@@ -43,24 +43,24 @@
 #}
 
 # Good Policy
-#data "aws_iam_policy_document" "bw-sentinel-policy" {
-#  statement {
-#    effect    = "Deny"
-#    actions   = ["s3:*"]
-#    resources = [
-#      "arn:aws:s3:::bw-sentinel/*",
-#      "arn:aws:s3:::bw-sentinel"
-#    ]
-#
-#    principals {
-#      type        = "*"
-#      identifiers = ["*"]
-#    }
-#
-#    condition {
-#      test     = "Bool"
-#      variable = "aws:SecureTransport"
-#      values   = ["false"]
-#    }
-#  }
-#}
+data "aws_iam_policy_document" "bw-sentinel-policy" {
+  statement {
+    effect    = "Deny"
+    actions   = ["s3:*"]
+    resources = [
+      "arn:aws:s3:::bw-sentinel/*",
+      "arn:aws:s3:::bw-sentinel"
+    ]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
