575e601df299656587e0fda8687c50d322f552e7
Brad Wise <brad@foghornconsulting.com>
2019-12-18 15:12:22 -0600

    fixes

--- s3.tf
+++ s3.tf
@@ -2,23 +2,23 @@
 #  description             = "This key is used to encrypt bucket objects"
 #  deletion_window_in_days = 7
 #}
-#
-#resource "aws_s3_bucket" "bw-sentinel" {
-#    bucket = "bw-sentinel"
-#    region = "us-east-1"
-#
-#    server_side_encryption_configuration {
-#        rule {
-#          apply_server_side_encryption_by_default {
-#            kms_master_key_id = "${aws_kms_key.bw-sentinel.arn}"
-#            # Good algorithm
-#            sse_algorithm = "aws:kms"
-#            # Bad algorithm
-#            #sse_algorithm = "AES256"
-#         }
-#       }
-#    }
-#}
+
+resource "aws_s3_bucket" "bw-sentinel" {
+    bucket = "bw-sentinel"
+    region = "us-east-1"
+
+    server_side_encryption_configuration {
+        rule {
+          apply_server_side_encryption_by_default {
+            kms_master_key_id = "${aws_kms_key.bw-sentinel.arn}"
+            # Good algorithm
+            sse_algorithm = "aws:kms"
+            # Bad algorithm
+            #sse_algorithm = "AES256"
+         }
+       }
+    }
+}
 
 resource "aws_s3_bucket_policy" "airbnb-brad-internal-only" {
     bucket = "${aws_s3_bucket.bw-sentinel.id}"
@@ -45,8 +45,8 @@ resource "aws_s3_bucket_policy" "airbnb-brad-internal-only" {
 # Good Policy
 data "aws_iam_policy_document" "bw-sentinel-policy" {
   statement {
-    effect    = "Deny"
-    actions   = ["s3:*"]
+    effect    = "Allow"
+    actions   = ["s3:PutObjectACL"]
     resources = [
       "arn:aws:s3:::bw-sentinel/*",
       "arn:aws:s3:::bw-sentinel"
@@ -55,12 +55,6 @@ data "aws_iam_policy_document" "bw-sentinel-policy" {
     principals {
       type        = "*"
       identifiers = ["*"]
-    }
-
-    condition {
-      test     = "Bool"
-      variable = "aws:SecureTransport"
-      values   = ["false"]
     }
   }
 }
