57f3a6ab0342776b0c418da72961aa6f45abb14b
Brad Wise <brad@foghornconsulting.com>
2019-12-19 10:15:19 -0600

    back to how it was before

--- s3.tf
+++ s3.tf
@@ -69,17 +69,17 @@ resource "aws_s3_bucket_policy" "bw-sentinel-1-policy" {
     policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
 }
 
-#resource "aws_s3_bucket" "bw-sentinel-2" {
-#    bucket = "bw-sentinel-2"
-#    region = "us-east-1"
-#}
-#
-#resource "aws_s3_bucket_policy" "bw-sentinel-2-policy" {
-#    bucket = "${aws_s3_bucket.bw-sentinel-2.id}"
-#    policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
-#}
-
-resource "aws_s3_bucket" "bw-sentinel-3" {
-    bucket = "bw-sentinel-3"
+resource "aws_s3_bucket" "bw-sentinel-2" {
+    bucket = "bw-sentinel-2"
     region = "us-east-1"
 }
+
+resource "aws_s3_bucket_policy" "bw-sentinel-2-policy" {
+    bucket = "${aws_s3_bucket.bw-sentinel-2.id}"
+    policy = "${data.aws_iam_policy_document.bw-sentinel-policy.json}"
+}
+
+#resource "aws_s3_bucket" "bw-sentinel-3" {
+#    bucket = "bw-sentinel-3"
+#    region = "us-east-1"
+#}
