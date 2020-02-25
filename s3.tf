bf72ed07dab7e29c70bc64bee498726503573420
Brad Wise <brad@foghornconsulting.com>
2020-02-25 09:27:06 -0600

    add relative path reference to module call

--- s3.tf
+++ s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "brad-wise-main-region-data2" {
 }
 
 module "brad-wise-dr-region-data2" {
-  source                = "modules/bucket"
+  source                = "./modules/bucket"
   bucket_name           = "brad-wise-dr-region-data2"
   inventory_bucket_name = "${aws_s3_bucket.brad-wise-main-region-data2.id}"
   destroy_resources     = true
