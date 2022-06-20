06b9295e5bd8cb0b24d3bb2832c5fb07cc5d73ca
Brad Wise <>
2022-06-20 09:51:04 -0500

    Remove remote state

--- remote.tf
+++ remote.tf
@@ -1,9 +1,9 @@
-data "terraform_remote_state" "buckets" {
-  backend = "s3"
-
-  config = {
-    bucket  = "bw-terraform-state-us-east-1"
-    key     = "brad-vault"
-    region  = "us-east-1"
-  }
-}
+#data "terraform_remote_state" "buckets" {
+#  backend = "s3"
+#
+#  config = {
+#    bucket  = "bw-terraform-state-us-east-1"
+#    key     = "brad-vault"
+#    region  = "us-east-1"
+#  }
+#}
